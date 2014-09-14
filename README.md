3ee90d11b1e2adbd8ce2d03bc6b12c8c2b8e4333
SO <so@dev.so>
2014-09-14 04:52:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# SSH Keychain
+Inspired by Jeff Lindsay's [Keychain.io](https://github.com/progrium/keychain.io), but written for Node.js. This project is much simpler, it doesn't offer as many options as Keychain.io and it doesn't require any third-party storage. Oh and you will have to host it yourself ;)
+
+Bash
+----
+Upload your default SSH key:
+
+```bash
+curl -s ssh.your-domain.tld/<email>/upload | bash
+```
+
+Install your key into authorized_keys:
+
+```bash
+curl -s ssh.your-domain.tld/<email>/install | bash
+```
+
+URLS
+----
+
+```
+ssh.your-domain.tld/<email>/upload
+ssh.your-domain.tld/<email>/install
+ssh.your-domain.tld/<email>/fingerprint
+```
+
+Credits
+-------
+This project couldn't do what it does without these node packages:
+
+- [express](https://www.npmjs.org/package/express)
+- [body-parser](https://www.npmjs.org/package/body-parser)
+- [ejs](https://www.npmjs.org/package/ejs)
+- [nedb](https://www.npmjs.org/package/nedb)
+- [ssh-fingerprint](https://www.npmjs.org/package/ssh-fingerprint)
+
+License
+-------
+
+Copyright (c) 2014, Steve Ottoz
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
---
+++ app.js
@@ -0,0 +1,94 @@
+var port = process.env.PORT || 8088,
+	express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	fingerprint = require('ssh-fingerprint'),
+	Datastore = require('nedb'),
+	db = new Datastore({ filename: 'data/db', autoload: true }),
+	pjson = require('./package.json');
+	
+app.use(bodyParser.json());
+
+// get upload script
+app.get('/:email/upload', function(req,res){
+	res.contentType('text/plain');
+	db.find({ email: req.params.email }, function (err, docs) {
+		if (!docs.length) {
+			res.render('upload.ejs', {url: req.protocol + '://' + req.get('host') + '/' + req.params.email, keypath: req.query.keypath});
+		}
+		else {
+			res.send('echo -e "\\033[31mSorry I just can\'t do it!\\033[0m"');
+		}
+	});
+});
+
+// upload ssh key
+app.put('/:email', function(req,res){
+	res.contentType('text/plain');
+	db.find({ email: req.params.email }, function (err, docs) {
+		if (!docs.length) {
+			db.insert({email: req.params.email, key: req.body.key, created: Date.now()}, function (err, newDoc){
+				if (err === null) {
+
+				}
+			});
+		}
+	});
+	res.end();
+});
+
+// get ssh key
+app.get('/:email', function(req,res){
+	res.contentType('text/plain');
+	db.find({ email: req.params.email }, function (err, docs) {
+		if (docs.length === 1) {
+			res.send(docs[0].key);
+		}
+		else {
+			res.send('No key found.');
+		}
+	});
+});
+
+// get install script
+app.get('/:email/install', function(req,res){
+	res.contentType('text/plain');
+	db.find({ email: req.params.email }, function (err, docs) {
+		if (docs.length === 1) {
+			res.render('install.ejs', {key: docs[0].key});
+		}
+		else {
+			res.send('echo -e "\\033[31mNo key to install.\\033[0m"');
+		}
+	});
+});
+
+// get ssh key fingerprint
+app.get('/:email/fingerprint', function(req,res){
+	res.contentType('text/plain');
+	db.find({ email: req.params.email }, function (err, docs) {
+		if (docs.length === 1) {
+			res.send(fingerprint(docs[0].key) + "\n");
+		}
+		else {
+			res.send('No key found.');
+		}
+	});
+});
+
+// 404 error handling
+app.use(function(req, res, next){
+	res.contentType('text/plain');
+	res.send('Nothing found.');
+});
+
+// 500 error handling
+app.use(function(err, req, res, next){
+	res.contentType('text/plain');
+	res.send('Something went wrong.');
+});
+
+// start server
+var server = app.listen(port, function() {
+    console.log('Listening on port %d', server.address().port);
+});
